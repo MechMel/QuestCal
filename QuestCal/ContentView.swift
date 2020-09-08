f28aff40d3ecc95ae38fbc0397270034597916e7
MechMel <MelchiahMauck@gmail.com>
2020-09-07 23:35:05 -0700

    Initial Commit

---
+++ QuestCal/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuestCal
+//
+//  Created by Jedidiah Mauck on 9/7/20.
+//  Copyright © 2020 Melchiah Mauck. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
